e0a39f5246802661d6449e2254eb221bf30f9e20
Golthi Venkatacharyulu <119730058+venkatesh1545@users.noreply.github.com>
2024-09-29 23:10:21 +0530

    Time: 97 ms (56.07%), Space: 54.3 MB (73.06%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -0,0 +1,35 @@
+class AllOne {
+public:
+    unordered_map<string, int> freq;
+    set<pair<int,string>> st;
+    AllOne() {
+    }
+    
+    void inc(string key) {
+        st.erase({freq[key],key});
+        freq[key]++;
+
+        st.insert({freq[key], key});
+    }
+    
+    void dec(string key) {
+        st.erase({freq[key],key});
+        freq[key]--;
+
+        if(freq[key] > 0){
+            st.insert({freq[key], key});
+        }
+    }
+    
+    string getMaxKey() {
+        if(st.size()==0){return "";}
+        string ans = prev(st.end())->second;
+        return ans;
+    }
+    
+    string getMinKey() {
+        if(st.size()==0){return "";}
+        string ans = st.begin()->second;
+        return ans;
+    }
+};
